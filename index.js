6d4f985dbd80e95ee887462e0076179ae324ed72
Loïc Zeiss <loiczeiss@gmail.com>
2024-05-29 11:59:11 +0200

    resetting architecture

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(express.json())
-const data = require('../data.json')
+const data = require('./data.json')
 app.use(cors());
 
 // REST API to get all products details at once
@@ -17,3 +17,5 @@ app.get("/", (req, res) => {
 app.listen(5000, () => {
     console.log('Server started on port 5000');
 });
+
+module.exports = app;
