f469d19a74ddffb22a104dae0f2f8eb53e369178
Loïc Zeiss <loiczeiss@gmail.com>
2024-05-30 18:13:41 +0200

    added region param

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-app.use(express.json())
-const data = require('./data.json')
+const _ = require('lodash'); // Import lodash for filtering
+app.use(express.json());
+const data = require('./data.json');
 app.use(cors());
 
-// REST API to get all products details at once
-// With this api the frontend will only get the data
-// The frontend cannot modify or update the data 
-// Because we are only using the GET method here.
-
 app.get("/", (req, res) => {
-    res.json(data)
+    let filteredData = data;
+
+    // Check if region query parameter is present
+    if (req.query.region) {
+        const region = req.query.region.toLowerCase();
+        filteredData = _.filter(data, country => country.region.toLowerCase() === region);
+    }
+
+    res.json(filteredData);
 });
 
 app.listen(5000, () => {
